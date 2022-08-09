99f6d2aa8a209715a8d24aa88ffba159216b124c
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-08-09 17:26:59 -0300

    Correção icone de busca na home

--- app/src/components/forms/HomeBusca/index.js
+++ app/src/components/forms/HomeBusca/index.js
@@ -1,26 +1,29 @@
 import styled from "styled-components";
 
 const HomeBusca = styled.form `
-    width: 346px;
+    width: 390px;
     display: flex;
     align-items: center;
+    justify-content: center;
     margin-top: 20px;
 
     input {
-        width: 100%;
-        height: 44px;
+        width: 332px;
+        height: 41px;
+        margin-left: 19px;
         border: 1.5px solid var(--azul);
         border-radius: var(--botao);
         padding: 0;
         padding-left: 14px;
     }
 
+    label {
+        transform: translateX(-34px);
+        cursor: text;
+    }
+
     .busca {
         width: 22px;
-        height: 22px;
-        position: absolute;
-        transform: translate(-49px, -11px);
-        cursor: text;
     }
 
 `
